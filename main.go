fcc0442ae6570ce96c3a59004cdd199568083acb
Luke Brictson <lbrictson@thecitybase.com>
2019-04-03 21:01:15 -0500

    more log

--- main.go
+++ main.go
@@ -13,13 +13,7 @@ var lg = logger.NewPackageLogger("main",
 )
 
 func main() {
-	// Read DHT11 sensor data from pin 4, retrying 10 times in case of failure.
-	// You may enable "boost GPIO performance" parameter, if your device is old
-	// as Raspberry PI 1 (this will require root privileges). You can switch off
-	// "boost GPIO performance" parameter for old devices, but it may increase
-	// retry attempts. Play with this parameter.
-	// Note: "boost GPIO performance" parameter is not work anymore from some
-	// specific Go release. Never put true value here.
+	logger.ChangePackageLogLevel("dht", logger.InfoLevel)
 	temperature, humidity, retried, err :=
 		dht.ReadDHTxxWithRetry(dht.DHT22, 22, false, 10)
 	if err != nil {
