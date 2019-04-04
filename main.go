e37fe77a92973f6e478f761421778be3738e9a3d
Luke Brictson <lbrictson@thecitybase.com>
2019-04-03 21:04:44 -0500

    fun

--- main.go
+++ main.go
@@ -14,12 +14,24 @@ var lg = logger.NewPackageLogger("main",
 
 func main() {
 	logger.ChangePackageLogLevel("dht", logger.InfoLevel)
-	temperature, humidity, retried, err :=
+	// temperature, humidity, retried, err :=
+	// 	dht.ReadDHTxxWithRetry(dht.DHT22, 22, false, 10)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// // Print temperature and humidity
+	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+	// 	temperature, humidity, retried)
+	t, h := getSensorData()
+	fmt.Printf("Temp is %v, humidity is: %v", t, h)
+	return
+}
+
+func getSensorData() (float32, float32) {
+	temperature, humidity, _, err :=
 		dht.ReadDHTxxWithRetry(dht.DHT22, 22, false, 10)
 	if err != nil {
 		log.Fatal(err)
 	}
-	// Print temperature and humidity
-	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
-		temperature, humidity, retried)
+	return temperature, humidity
 }
