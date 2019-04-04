0dccf214315d1b51fca9736f6819dbe82e46ecf0
Luke Brictson <lbrictson@thecitybase.com>
2019-04-03 21:06:43 -0500

    loop it

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"time"
 
 	"github.com/d2r2/go-dht"
 	logger "github.com/d2r2/go-logger"
@@ -13,7 +14,7 @@ var lg = logger.NewPackageLogger("main",
 )
 
 func main() {
-	logger.ChangePackageLogLevel("dht", logger.InfoLevel)
+	logger.ChangePackageLogLevel("dht", logger.ErrorLevel)
 	// temperature, humidity, retried, err :=
 	// 	dht.ReadDHTxxWithRetry(dht.DHT22, 22, false, 10)
 	// if err != nil {
@@ -22,8 +23,11 @@ func main() {
 	// // Print temperature and humidity
 	// fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
 	// 	temperature, humidity, retried)
-	t, h := getSensorData()
-	fmt.Printf("Temp is %v, humidity is: %v\n", t, h)
+	for {
+		t, h := getSensorData()
+		fmt.Printf("Temp is %v, humidity is: %v\n", t, h)
+		time.Sleep(10 * time.Second)
+	}
 	return
 }
 
