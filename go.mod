4fd2b8546276d0174bcff30e491ae56abff7982b
Luke Brictson <lbrictson@thecitybase.com>
2019-04-03 22:46:29 -0500

    influx

--- go.mod
+++ go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/d2r2/go-dht v0.0.0-20190315200754-2b4976dbf5ce
 	github.com/d2r2/go-logger v0.0.0-20181221090742-9998a510495e
 	github.com/d2r2/go-shell v0.0.0-20181221082743-874cb5c847b3 // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/influxdata/influxdb v1.7.5
+	github.com/influxdata/platform v0.0.0-20190117200541-d500d3cf5589 // indirect
 	github.com/stianeikeland/go-rpio/v4 v4.4.0 // indirect
 )
--- main.go
+++ main.go
@@ -7,7 +7,11 @@ import (
 
 	"github.com/d2r2/go-dht"
 	logger "github.com/d2r2/go-logger"
+	"github.com/influxdata/influxdb/client/v2"
 )
+
+var lastHumid float64
+var lastTemp float64
 
 var lg = logger.NewPackageLogger("main",
 	logger.InfoLevel,
@@ -18,6 +22,7 @@ func main() {
 	for {
 		t, h := getSensorData()
 		fmt.Printf("Temp is %v, humidity is: %v\n", t, h)
+		writeInflux(t, h)
 		time.Sleep(10 * time.Second)
 	}
 	return
@@ -27,7 +32,45 @@ func getSensorData() (float32, float32) {
 	temperature, humidity, _, err :=
 		dht.ReadDHTxxWithRetry(dht.DHT22, 22, false, 10)
 	if err != nil {
-		log.Fatal(err)
+		log.Error(err)
+		return lastTemp, lastHumid
 	}
+	lastHumid = humidity
+	lastTemp = temperature
 	return temperature, humidity
+}
+
+func writeInflux(temp float32, hum float32) {
+	// Make client
+	c, err := client.NewHTTPClient(client.HTTPConfig{
+		Addr: "http://localhost:8086",
+	})
+	if err != nil {
+		fmt.Println("Error creating InfluxDB Client: ", err.Error())
+		return
+	}
+	defer c.Close()
+
+	// Create a new point batch
+	bp, _ := client.NewBatchPoints(client.BatchPointsConfig{
+		Database:  "pistats",
+		Precision: "s",
+	})
+
+	// Create a point and add to batch
+	tags := map[string]string{"weather": "current"}
+	fields := map[string]interface{}{
+		"temp":     temp,
+		"humidity": hum,
+	}
+	pt, err := client.NewPoint("weather", tags, fields, time.Now())
+	if err != nil {
+		fmt.Println("Error: ", err.Error())
+		return
+	}
+	bp.AddPoint(pt)
+
+	// Write the batch
+	c.Write(bp)
+	return
 }
